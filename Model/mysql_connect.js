c9e1ccacf246ba827d6ba5623e5656e993b65c24
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-17 19:02:03 -0400

    Update mysql_connect.js

--- Model/mysql_connect.js
+++ Model/mysql_connect.js
@@ -27,3 +27,63 @@ mysql_connection.connect((error) => {
 // [Application]: Run the app and listen at port 7000.
 const port = 7000;
 application.listen(port, () => console.log('listening on port 7000...'));
+
+
+// Login Home Page:
+application.get('/', (req, res) =>
+   // res.sendFile(path, [, options], [, function]): Is used to transfer the files at the given path.
+   // The __dirname in a node script returns the path of the folder where the current JavaScript file resides (gets directory name of the currently executing file where './'gives the current working directory).
+   res.sendFile(__dirname + '/login.html');
+});
+
+// Register Form:
+application.get('/register-form', (req, res) =>
+   res.sendFile(__dirname + '/register-form.html');
+});
+
+
+// Register to database:
+application.post('/register', urlencodedParser, (req, res) => {
+   var username = req.body.username;
+   var password = req.body.password;
+   var first_name = req.body.first_name;
+   var last_name = req.body.last_name;
+   
+   var bcrypt = require('bcrypt.js');
+   var salt = bcrypt.genSaltSync(10);
+   var hashed_password = bcrypt.hashSync(req.body.password, salt);
+   
+   mysql_connection.query("insert into administrators (username, password, firstName, lastName) values ('"+ username + "', '"+ hashed_password + "',  '"+ first_name + "', '"+ last_name + "')", 
+      (error, rows, fields) => {
+         if(!error)
+           res.send(`Welcome {$username} to join us`);
+         else 
+           console.log(error);
+      });
+});
+
+application.post('/login', urlencodedParser, (req, res) => {
+   var username = req.body.username;
+   var password = req.body.password;
+   var first_name = req.body.first_name;
+   var last_name = req.body.last_name;
+   
+   var bcrypt = require('bcrypt.js');
+   
+   mysql_connection.query('select * from administrators where username = ?', [username], 
+      (error, rows, fields) => {
+         if(!error){
+            var passwd = rows[0].password;
+            bcrypt.compare(password, passwd, function(error, result){
+               if(result)
+                 res.send(`${first_name} ${last_name}, welcome to our site!`);
+               else 
+                 res.sendFile(__dirname + '/login.html');
+            });
+         }
+         else{ 
+           // Ask the user to login again.
+           res.sendFile(__dirname + '/login.html');
+         }
+      });
+});
