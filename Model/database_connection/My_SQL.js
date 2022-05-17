41e58acec30fe7fb97bc9b1016302836cd264e27
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-17 18:53:52 -0400

    Create My_SQL.js

---
+++ Model/database_connection/My_SQL.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+const express = require('express');
+const body_parser = require('body-parser');
+
+var application = express();
+application.use(body_parser.json());
+
+var mysql_connection = mysql.createConnection({
+   host: 'localhost',
+   user: 'iw3htp',
+   password: 'password',
+   database: 'products',
+   multipleStatement: true
+});
+
+mysql_connection.connect((error) => {
+   if(!error)
+     console.log('Connection Successful.');
+   else
+     console.log('Connection Failed.');
+});
+
+const port_number = 7000;
+application.listen(port_number, () => console.log('listening on port 7000'));
