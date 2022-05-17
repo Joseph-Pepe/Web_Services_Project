6562c39941470f6916dc4d8b3040f1f0bf705033
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-17 19:26:41 -0400

    Create insert.js

---
+++ insert.js
@@ -0,0 +1,13 @@
+application.get('/insert', function(req, res){
+   var html = ";
+   html += "<body>";
+   html += "<form action='/process-insert-form' method='post' name='form1'>";
+   html += "<label>Book Title</label><input type='text' name='title'/>>";
+   html += "<label>Category:</label><input type='text' name='category'/>";
+   html += "<label>ISBN:</label><input type='text' name='isbn'/>";
+   html += "<input type='submit' value='submit'/>";
+   html += "<input type='reset' value='reset'/>";
+   html += "</form>";
+   html += "</body>";
+   res.send(html);
+});
