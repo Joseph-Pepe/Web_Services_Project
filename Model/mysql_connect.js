7849153e4ab31a2c6228c4a5f613467074f69d8f
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-17 18:57:28 -0400

    Update mysql_connect.js

--- Model/mysql_connect.js
+++ Model/mysql_connect.js
@@ -1,18 +1,22 @@
+// [Packages]: Gets the required packages.
 const mysql = require('mysql');
 const express = require('express');
 const body_parser = require('body-parser');
 
+var urlencodedParser = body_parser.urlencoded({extended: true});
 var application = express();
 application.use(body_parser.json());
 
+// [Connection]: Need MySQL database connection.
 var mysql_connection = mysql.createConnection({
    host: 'localhost',
-   user: 'username',
-   password: 'password',
-   database: 'database_name',
+   user: 'mgs_user',
+   password: 'pa55word',
+   database: 'my_guitar_shop2',
    multipleStatement: true
 });
 
+// [Check Connection]: Check if MySQL started and can be accessed.
 mysql_connection.connect((error) => {
    if(!error)
      console.log('Connection Successful.');
@@ -20,5 +24,6 @@ mysql_connection.connect((error) => {
      console.log('Connection Failed.');
 });
 
-const port_number = 7000;
-application.listen(port_number, () => console.log('listening on port 7000'));
+// [Application]: Run the app and listen at port 7000.
+const port = 7000;
+application.listen(port, () => console.log('listening on port 7000...'));
