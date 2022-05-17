57f8e1935e93908fc18f3fc45aab84a19500276c
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-17 19:32:01 -0400

    Update mysql_connect.js

--- model/mysql_connect.js
+++ model/mysql_connect.js
@@ -41,6 +41,51 @@ application.get('/register-form', (req, res) =>
    res.sendFile(__dirname + '/register-form.html');
 });
 
+// Register account into database:
+application.post('/register', urlencodedParser, (req, res) => {
+   var username = req.body.username;
+   var password = req.body.password;
+   var first_name = req.body.first_name;
+   var last_name = req.body.last_name;
+   
+   var bcrypt = require('bcrypt.js');
+   var salt = bcrypt.genSaltSync(10);
+   var hashed_password = bcrypt.hashSync(req.body.password, salt);
+   
+   mysql_connection.query("insert into administrators (username, password) values ('"+ username + "', '"+ hashed_password + "')", 
+      (error, rows, fields) => {
+         if(!error)
+           res.send(`Welcome {$username} to join us`);
+         else 
+           console.log(error);
+      });
+});
+
+// Login to account.
+application.post('/login', urlencodedParser, (req, res) => {
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   var bcrypt = require('bcrypt.js');
+   
+   mysql_connection.query('select * from administrators where username = ?', [username], 
+      (error, rows, fields) => {
+         if(!error){
+            var passwd = rows[0].password;
+            bcrypt.compare(password, passwd, function(error, result){
+               if(result)
+                 res.send(`${username}, welcome to our site!`);
+               else 
+                 res.sendFile(__dirname + '/login.html');
+            });
+         }
+         else{ 
+           // Ask the user to login again.
+           res.sendFile(__dirname + '/login.html');
+         }
+      });
+});
+
 // Insert Product Form
 application.get('/insert', function(req, res){
    var html = ";
@@ -66,55 +111,5 @@ application.post('/process-insert-form', urlencodedParser, function(req, res){
            res.send(rows);
          else 
            console.log(error);
-      });
-});
-
-
-
-// Register to database:
-application.post('/register', urlencodedParser, (req, res) => {
-   var username = req.body.username;
-   var password = req.body.password;
-   var first_name = req.body.first_name;
-   var last_name = req.body.last_name;
-   
-   var bcrypt = require('bcrypt.js');
-   var salt = bcrypt.genSaltSync(10);
-   var hashed_password = bcrypt.hashSync(req.body.password, salt);
-   
-   mysql_connection.query("insert into administrators (username, password) values ('"+ username + "', '"+ hashed_password + "')", 
-      (error, rows, fields) => {
-         if(!error)
-           res.send(`Welcome {$username} to join us`);
-         else 
-           console.log(error);
-      });
-});
-
-
-
-
-// Login to account.
-application.post('/login', urlencodedParser, (req, res) => {
-   var username = req.body.username;
-   var password = req.body.password;
-   
-   var bcrypt = require('bcrypt.js');
-   
-   mysql_connection.query('select * from administrators where username = ?', [username], 
-      (error, rows, fields) => {
-         if(!error){
-            var passwd = rows[0].password;
-            bcrypt.compare(password, passwd, function(error, result){
-               if(result)
-                 res.send(`${username}, welcome to our site!`);
-               else 
-                 res.sendFile(__dirname + '/login.html');
-            });
-         }
-         else{ 
-           // Ask the user to login again.
-           res.sendFile(__dirname + '/login.html');
-         }
       });
 });
