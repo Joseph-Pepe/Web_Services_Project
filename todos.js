b8f3ca2468007ea5c54ed610c209d4b60c247cf0
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-30 14:10:50 -0400

    Update todos.js

--- todos.js
+++ todos.js
@@ -13,6 +13,20 @@ function load_todos(){
   
    // Load all keys:
    for(var index = 0; index < length; ++index){
-      tags[index] = localStorage.key(index); 
+      todo_tags[index] = localStorage.key(index); 
    }
+   
+   todo_tags.sort();
+   
+   var markup = "<ul>";
+   
+   for(var tag in todo_tags){
+      var todo = localStorage.getItem(todo_tags[tag]);
+      markup += "<li><span>" + todo_tags[tag] + "</li></span>" 
+             + "<input id = '" + todo_tags[tag] + "' type = 'button' " + "value = 'edit' onclick = 'editTodo(id)'>"
+             + "<input id = '" + todo_tags[tag] + "' type = 'button' " + "value = 'delete' onclick = 'deleteTodo(id)'>";
+   }
+   
+   var markup += "</ul>";
+   document.getElementById("todos").innerHTML = markup;
 }
