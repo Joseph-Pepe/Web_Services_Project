5296ae892e41f880671f4430a9d9c47509beba03
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-30 16:49:43 -0400

    Update todos.js

--- todos.js
+++ todos.js
@@ -1,36 +1,5 @@
 
 
-// Loads previously saved todos and displays them in the page.
-function load_todos(){
-  
-   if(!sessionStorage.getItem("herePreviously")){
-      // Used to display welcome message.
-      sessionStorage.setItem("herePreviously", "true");
-      document.getElementById("welcomeMessage").innerHTML = "Welcome to Todos Application";
-   }
-   
-   var length = localStorage.length;
-   todo_tags = [];
-  
-   // Load all keys:
-   for(var index = 0; index < length; ++index){
-      todo_tags[index] = localStorage.key(index); 
-   }
-   
-   todo_tags.sort();
-   
-   var markup = "<ul>";
-   
-   for(var tag in todo_tags){
-      var person_assigned = localStorage.getItem(todo_tags[tag]);
-      markup += "<li><span>" + person_assigned + ": " + todo_tags[tag] + "</span>" 
-             + "<input id = '" + todo_tags[tag] + "' type = 'button' " + "value = 'edit' onclick = 'edit_todo(id)'>"
-             + "<input id = '" + todo_tags[tag] + "' type = 'button' " + "value = 'delete' onclick = 'delete_todo(id)'>";
-   }
-   
-   var markup += "</ul>";
-   document.getElementById("todos").innerHTML = markup;
-}
 
 // Deletes all key/value pairs from localStorage.
 function clear_all_todos(){
@@ -75,43 +44,40 @@ function start(){
 // Array of tags for todos.
 var todo_tags;
 
-// Loads previously saved searches and displays them in the page.
-function loadSearches() {
+// Loads previously saved todos and displays them in the page.
+function load_todos() {
     // Used to determine whether the user has already visited the page during this browsing session.
    if (!window.sessionStorage.getItem( "herePreviously" )){
       sessionStorage.setItem( "herePreviously", "true" );
-      document.getElementById( "welcomeMessage" ).innerHTML = 
-         "Welcome to the Favorite Twitter Searches App";
+      document.getElementById( "welcomeMessage" ).innerHTML = "Welcome to Todos Application";
    } 
 
-   var length = localStorage.length; // number of key-value pairs
-   todo_tags = []; // create empty array
+   var length = localStorage.length; 
+   todo_tags = []; 
 
-   // load all keys
-   for (var index = 0; index < length; ++index) 
-   {
+   // Load all keys.
+   for (var index = 0; index < length; ++index) {
       todo_tags[index] = localStorage.key(index);
-   } // end for
+   } 
 
    todo_tags.sort(); // sort the keys
 
-   var markup = "<ul>"; // used to store search link markup
+   var markup = "<ul>"; 
 
-   // build list of links
-   for (var tag in tags) 
-   {
-      var query = url + localStorage.getItem(tags[tag]);
-      markup += "<li><span><a href = '" + query + "'>" + tags[tag] + 
-         "</a></span>" +
-         "<input id = '" + tags[tag] + "' type = 'button' " + 
-            "value = 'Edit' onclick = 'editTag(id)'>" +
-         "<input id = '" + tags[tag] + "' type = 'button' " + 
-            "value = 'Delete' onclick = 'deleteTag(id)'>";
-   } // end for
+   // Build list of todos.
+   for (var todo in todo_tags){
+      var person_assigned = localStorage.getItem(todo_tags[todo]);
+      markup += "<li><span>" + person_assigned + ": " + todo_tags[todo] + 
+         "</span>" +
+         "<input id = '" + todo_tags[todo] + "' type = 'button' " + 
+            "value = 'Edit' onclick = 'edit_todo(id)'>" +
+         "<input id = '" + todo_tags[todo] + "' type = 'button' " + 
+            "value = 'Delete' onclick = 'delete_todo(id)'>";
+   }
 
    markup += "</ul>";
-   document.getElementById("searches").innerHTML = markup;
-} // end function loadSearches
+   document.getElementById("todos").innerHTML = markup;
+}
 
 // deletes all key-value pairs from localStorage
 function clearAllSearches() 
