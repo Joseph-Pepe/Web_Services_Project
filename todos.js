be66cc7888fed677550bd80760f04334474ab9cc
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-30 14:00:42 -0400

    Create todos.js

---
+++ todos.js
@@ -0,0 +1,19 @@
+// Array of tags for todos.
+var todo_tags;
+
+// Loads previously saved todos and displays them in the page.
+function load_todos(){
+   if(!sessionStorage.getItem("herePreviously")){
+      sessionStorage.setItem("herePreviously", true);
+      document.getElementById("welcomeMessage").innerHTML = "Welcome to Todos Application";
+   }
+   
+   var length = localStorage.length;
+   todo_tags = [];
+  
+   // Load all keys:
+   for(var index = 0; index < length; ++index){
+      tags[index] = localStorage.key(index); 
+   }
+  
+}
