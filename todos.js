ef2f3263610c201977932c1c83a9e18f2a60ba12
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-30 18:04:36 -0400

    Update todos.js

--- todos.js
+++ todos.js
@@ -47,7 +47,7 @@ function clear_all_todos() {
 function save_todo() {
    var query = document.getElementById("query");
    var tag = document.getElementById("todo");
-   localStorage.setItem(tag.value, "rt"); 
+   localStorage.setItem(tag.value, query.value); 
    tag.value = ""; 
    query.value = ""; 
    load_todos(); 
