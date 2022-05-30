69d0b98ca5f5d2d25ab0afec6df4d814ccb86279
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-30 17:32:23 -0400

    Update todos.js

--- todos.js
+++ todos.js
@@ -1,37 +1,4 @@
 /*
-function load_todos() {
-   // Used to determine whether the user has already visited the page during this browsing session.
-   if (!window.sessionStorage.getItem( "herePreviously" )){
-      sessionStorage.setItem( "herePreviously", "true" );
-      document.getElementById( "welcomeMessage" ).innerHTML = "Welcome to Todos Application";
-   } 
-
-   var length = localStorage.length; 
-   todo_tags = []; 
-
-   // Load all keys.
-   for (var index = 0; index < length; ++index) {
-      todo_tags[index] = localStorage.key(index);
-   } 
-
-   todo_tags.sort(); // sort the keys
-
-   var markup = "<ul>"; 
-
-   // Build list of todos.
-   for (var todo in todo_tags){
-      var person_assigned = localStorage.getItem(todo_tags[todo]);
-      markup += "<li><span>" + person_assigned + ": " + todo_tags[todo] + 
-         "</span>" +
-         "<input id = '" + todo_tags[todo] + "' type = 'button' " + 
-            "value = 'Edit' onclick = 'edit_todo(id)'>" +
-         "<input id = '" + todo_tags[todo] + "' type = 'button' " + 
-            "value = 'Delete' onclick = 'delete_todo(id)'>";
-   }
-
-   markup += "</ul>";
-   document.getElementById("todos").innerHTML = markup;
-}
 
 // Deletes all key-value pairs from localStorage
 function clear_all_todos(){
@@ -77,6 +44,7 @@ var todo_tags;
 
 // Loads previously saved todos and displays them in the page.
 function load_todos() {
+   // Used to determine whether the user has already visited the page during this browsing session.
    if (!window.sessionStorage.getItem( "herePreviously" )){
       sessionStorage.setItem( "herePreviously", "true" );
       document.getElementById( "welcomeMessage" ).innerHTML = "Welcome to Todos Application";
@@ -130,14 +98,14 @@ function save_todo()
 } // end function saveSearch
 
 // deletes a specific key-value pair from localStorage
-function delete_todo( todo ) 
+function delete_todo(todo) 
 {
-   localStorage.removeItem( todo );
+   localStorage.removeItem(todo);
    load_todos(); // reload searches
 } // end function deleteTag
 
 // display existing tagged query for editing
-function edit_todo( todo )
+function edit_todo(todo)
 {
    document.getElementById("query").value = localStorage[ todo ];
    document.getElementById("tag").value = todo;   
