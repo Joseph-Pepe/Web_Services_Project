f5d64bcab64b2c692c60e2fae258a0e8dc48745b
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-06-02 18:41:46 -0400

    Update todos.js

--- public/todos.js
+++ public/todos.js
@@ -1,14 +1,17 @@
+/* [Step #1]: Store and retrieve key/value pairs using HTML5 localStorage and sessionStorage. */
 // Array of tags for todos.
 var todo_tags;
 
 // Loads previously saved todos and displays them in the page.
 function load_todos() {
    // Used to determine whether the user has already visited the page during this browsing session.
+   // The getItem() recieves a name of the key, and returns the corresponding string value.
    if (!window.sessionStorage.getItem( "herePreviously" )){
       sessionStorage.setItem( "herePreviously", "true" );
       document.getElementById( "welcomeMessage" ).innerHTML = "Welcome to Todos Application";
    }
 
+   // Represents the number of key/value pairs stored.
    var length = localStorage.length;
    todo_tags = [];
 
@@ -17,9 +20,12 @@ function load_todos() {
       todo_tags[index] = localStorage.key(index);
    } 
 
+   
+   /* [Step #2]: Build a list of todos that are stored. */
    todo_tags.sort(); // sort the keys
 
-   var markup = "<ul>"; // used to store search link markup
+   // Used to store todos markup.
+   var markup = "<ul>"; 
 
    // Build list of todos.
    for (var todo in todo_tags){
@@ -30,10 +36,11 @@ function load_todos() {
          "<input id = '" + todo_tags[todo] + "' type = 'button' " + 
             "value = 'Delete' onclick = 'delete_todo(id)'>";
    }
-
+   
    markup += "</ul>";
    document.getElementById("todos").innerHTML = markup;
 } 
+
 
 // Deletes all key-value pairs from localStorage.
 function clear_all_todos() {
@@ -46,8 +53,12 @@ function save_todo() {
    var person = document.getElementById("person");
    var tag = document.getElementById("todo");
    localStorage.setItem(tag.value, person.value); 
+   
+   // Clear todo and person assigned.
    tag.value = ""; 
    person.value = ""; 
+   
+   // Reload searches.
    load_todos(); 
 } 
 
@@ -70,6 +81,8 @@ function start(){
    saveButton.addEventListener( "click", save_todo, false );
    var clearButton = document.getElementById( "clearButton" );
    clearButton.addEventListener( "click", clear_all_todos, false );
+   
+   // Load the previously saved todos.
    load_todos(); 
 } 
 
