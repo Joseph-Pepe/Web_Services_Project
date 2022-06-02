c3a5a8ee39d617ce17cb98281da48f50de41611e
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-06-02 13:36:59 -0400

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -7,7 +7,7 @@ USE csit437;
 CREATE TABLE accounts( 
    account_id        INT            NOT NULL   AUTO_INCREMENT, 
    username          VARCHAR(255)   NOT NULL, 
-   password          VARCHAR(50)    NOT NULL,
+   password          VARCHAR(255)   NOT NULL,
    PRIMARY KEY (account_id) 
 );
 
